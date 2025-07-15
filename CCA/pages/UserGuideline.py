39252f2c6a95b980fbe9d8e8adbaf183e256eda2
Muhammad Daniel Alif bin Jumairi <69742721+dxn1405@users.noreply.github.com>
2025-07-15 15:06:37 +0800

    Update UserGuideline.py

--- CCA/pages/UserGuideline.py
+++ CCA/pages/UserGuideline.py
@@ -2,10 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="User Guideline", layout="centered")
 
-# Add this at the top of each page file
-st.sidebar.title("Cervical Cancer Image Classification Using Vision Transformer")
-
-
 st.title("User Guideline")
 
 st.markdown("""
